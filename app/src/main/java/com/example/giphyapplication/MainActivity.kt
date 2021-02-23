d5a5948325bd9141afba30744fe18356c6abc7dc
SeungYooon <tmddbs4998@naver.com>
2021-02-24 01:22:41 +0900

    네비게이션 설정

---
+++ app/src/main/java/com/example/giphyapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.giphyapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigateToTrending()
+    }
+
+    private fun navigateToTrending() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.navigate(R.id.action_global_trendingFragment)
+    }
+}
